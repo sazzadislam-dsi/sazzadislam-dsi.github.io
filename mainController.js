46fbb408c9c2b2ab71c7516376332bf95b3744ac
LynAs <szlynas@gmail.com>
2016-03-24 09:55:15 +0600

    ooo

--- mainController.js
+++ mainController.js
@@ -1,7 +1,34 @@
 (function () {
     var app = angular.module("demoApp");
 
-    var mainController = function ($scope) {
+    var mainController = function ($scope, $http) {
+        var req = {
+            method: 'GET',
+            url: 'https://api.github.com/users/lynas',
+            headers: {
+                
+            }
+        };
+
+        $scope.makeCall = function () {
+            $scope.loading = true;
+            $http(req).then(function(response){
+                $scope.myWelcome = response.data.login;
+                $scope.loading = false;
+            });
+            /*$http.get("https://api.github.com/users/lynas")
+                .then(function(response) {
+                    $scope.myWelcome = response.data.login;
+                });*/
+
+            /*$http.get('https://api.github.com/users/lynas', {
+                headers: {
+                    "X-Poll-Interval": 20
+                }
+            }).success(function(response){
+                console.log("rrr : "+response)
+            });*/
+        }
 
     };
 
